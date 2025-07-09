500745494ed54d824ab03ae8d22ce5efee3f62db
Stephen Schaz <stephenschaz90@gmail.com>
2025-07-10 01:22:34 +0200

    Firebase linked / testNotes output in Clock Comp

--- src/app/analog-clock/analog-clock.ts
+++ src/app/analog-clock/analog-clock.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-analog-clock',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './analog-clock.html',
   styleUrl: './analog-clock.scss'
 })
@@ -14,9 +16,19 @@ export class AnalogClock implements OnInit {
   intervalId: any;
   currentWeekday: string = "";
   currentDate: string = "";
+  notes: any[] | undefined;
   @Input() clockFace: string = '/images/clockface/GoldenNumbers.png';
 
+  constructor(private firestore: Firestore) {}
+
   ngOnInit() {
+    const notesCollection = collection(this.firestore, 'profiles/webmaster1/notes');
+
+    collectionData(notesCollection, { idField: 'docId' }).subscribe(data => {
+      this.notes = data;
+      console.log('Notes from Firestore:', data);
+    });
+  
     this.runClock();
     this.getDateAndDay();
   }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,16 +5,33 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 
+// ✅ Firebase Imports
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+// ✅ Deine Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyBDL55GhF8tyUuohV7Xbyc3U90dWhUSjt8",
+  authDomain: "managment-dashboard-9d06b.firebaseapp.com",
+  projectId: "managment-dashboard-9d06b",
+  storageBucket: "managment-dashboard-9d06b.appspot.com",
+  messagingSenderId: "9119685140",
+  appId: "1:9119685140:web:0ac68a0e24e6ace1a0e501"
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-        providePrimeNG({
-            theme: {
-                preset: Aura
-            }
-        })
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
+    // ✅ Firebase & Firestore registrieren
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ]
 };
